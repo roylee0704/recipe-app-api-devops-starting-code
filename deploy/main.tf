817b089a6fbb7b23f9bc3958e32dc3b81d398336
roylee0704 <roylee0704@gmail.com>
2020-08-10 10:26:48 +0700

    added tags

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,14 +1,26 @@
 terraform {
-    backend "s3" {
-        bucket = "roy-recipe-app-devops-tfstate"
-        key = "roy-recipe-app.tfstate"
-        region = "ap-southeast-1"
-        encrypt = true
-        dynamodb_table = "roy-recipe-app-devops-tf-state-lock"
-    }
+  backend "s3" {
+    bucket         = "roy-recipe-app-devops-tfstate"
+    key            = "roy-recipe-app.tfstate"
+    region         = "ap-southeast-1"
+    encrypt        = true
+    dynamodb_table = "roy-recipe-app-devops-tf-state-lock"
+  }
 }
 
 provider "aws" {
-    region = "ap-southeast-1"
-    version = "~>2.54.0"
+  region  = "ap-southeast-1"
+  version = "~>2.54.0"
+}
+
+# local variables 
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}" # raad-dev
+
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    Owner       = var.contact
+    ManagedBy   = "Terraform"
+  }
 }
