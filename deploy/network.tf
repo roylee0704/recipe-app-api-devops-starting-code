674cdda44349e0042d979ccf3760a636ab7b84a7
roylee0704 <roylee0704@gmail.com>
2020-08-11 15:26:03 +0700

    added private subnet

--- deploy/network.tf
+++ deploy/network.tf
@@ -206,3 +206,86 @@ resource "aws_nat_gateway" "public_b" {
     map("Name", "${local.prefix}-public-b")
   )
 }
+
+###################################################
+# Private Subnets - Outbound internet access only #
+###################################################
+
+resource "aws_subnet" "private_a" {
+  # to reserve between 10.1.1.x and 10.1.9.x network 
+  # for public subnets - network growth in future.
+  cidr_block = "10.1.10.0/24"
+  vpc_id     = aws_vpc.main.id
+
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-a")
+  )
+}
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-a")
+  )
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route" "private_a_internet_out" {
+  route_table_id = aws_route_table.private_a.id
+
+
+  # we need to rely on the nat gateway 
+  # (which located in the public-*) in order
+  # to go out to outside world
+  nat_gateway_id = aws_nat_gateway.public_a.id
+
+  # this is a public network (internet)
+  # destination means outbound
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+
+resource "aws_subnet" "private_b" {
+  cidr_block = "10.1.11.0/24"
+  vpc_id     = aws_vpc.main.id
+
+  # use prefix techinque for name-spacing (isolation)
+  # prefix: aws-region-name
+  availability_zone = "${data.aws_region.current.name}b}"
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-b")
+  )
+}
+
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-b")
+  )
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
+
+resource "aws_route" "private_b_internet_out" {
+  route_table_id = aws_route_table.private_b.id
+
+  nat_gateway_id         = aws_nat_gateway.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+}
