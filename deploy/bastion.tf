3a19c43e1733d0f2d39c9ad1a89dd308771e8146
roylee0704 <roylee0704@gmail.com>
2020-08-15 13:08:29 +0700

    added instance profile for bastion

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -12,6 +12,33 @@ data "aws_ami" "amazon_linux" {
 }
 
 
+
+
+# role have access to ecr repo: pull down docker images
+resource "aws_iam_role" "bastion" {
+  name = "${local.prefix}-bastion"
+
+  # can be assumed by which principals? i.e: ec2, lambda, etc...
+  assume_role_policy = file("./templates/bastion/instance-profile-policy.json")
+  tags               = local.common_tags
+
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_attach_policy" {
+  role = aws_iam_role.bastion.name
+
+  # predefined by aws 
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# instance profile is a container to hold a role name.
+# Any role name that is placed under an instance profile will be used 
+# to run assume-role command internally by ec2.
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${local.prefix}-bastion-instance-profile"
+  role = aws_iam_role.bastion.name
+}
+
 # create resource
 resource "aws_instance" "bastion" {
   ami           = data.aws_ami.amazon_linux.id
@@ -21,6 +48,9 @@ resource "aws_instance" "bastion" {
   # you may add script to this attribute
   user_data = file("./templates/bastion/user-data.sh")
 
+  
+  # assign an instance profile to ec2
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
 
   # this is like metadata in kubernetes manifest
   # tags = {
