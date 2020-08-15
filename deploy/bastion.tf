8417b271593f4930d27234515645b9dd0ede70e9
roylee0704 <roylee0704@gmail.com>
2020-08-15 07:41:45 +0700

    added bastion user data script

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -1,3 +1,5 @@
+# bastion is a server that allows administrators to 
+# connect to our resources in the private subnet.
 
 # query resource
 data "aws_ami" "amazon_linux" {
@@ -14,11 +16,17 @@ data "aws_ami" "amazon_linux" {
 resource "aws_instance" "bastion" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
+
+  # ./ takes us to the root of a directory, "deploy"
+  # you may add script to this attribute
+  user_data = file("./templates/bastion/user-data.sh")
+
+
   # this is like metadata in kubernetes manifest
   # tags = {
   #   Name = "${local.prefix}-bastion" # raad-dev-bastion
   # }
-
+  #
   # for overriding purposes
   tags = merge(
     local.common_tags,
